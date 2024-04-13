aeca7ecaf2476591c75ee917473356f25bf3ff42
TankistPro <nakol.nayk@mail.ru>
2024-04-13 22:59:51 +0300

    Добавлен парсинг данных через скролл, формирование Excel-документа

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
-import requests
+import time
+
 from typing import Union
 from openpyxl import Workbook
+from selenium import webdriver
+from openpyxl.utils import get_column_letter
+from openpyxl.styles import Alignment, PatternFill, Border, Side
 from bs4 import Tag, ResultSet, BeautifulSoup as bs
 
 class CustomOlympicParser:
@@ -8,10 +12,20 @@ class CustomOlympicParser:
 
     def getRequestPage(self, url : str) -> Union[str, None]:
         """Getting HTML-markup from url. If URL bad - returned None"""
-        page = requests.get(url)
 
-        if page.status_code == 200:
-            return page.text
+        service = webdriver.ChromeService()
+        driver = webdriver.Chrome(service=service)
+        driver.get(url)
+
+        for i in range(20):
+            driver.execute_script(
+                "window.scrollTo(0, document.body.scrollHeight);")
+            time.sleep(1)
+
+        page = driver.page_source
+
+        if page != "":
+            return page
 
 
     def getNodeElementList(self, html_markup: str) -> Union[ResultSet, None]:
@@ -21,26 +35,75 @@ class CustomOlympicParser:
             soup = bs(markup=html_markup, features="html.parser")
             return soup.find_all("div", class_="fav_olimp olimpiada")
 
-    def parseDataFromNode(self, node: Tag) -> dict:
+    def parseDataFromNodeList(self, nodeList: dict) -> list:
         """Prepares data to save from node list"""
 
-        title = node.find("span", class_="headline")
-        raiting = node.find("span", class_="pl_rating")
-        description = node.find("a", class_="none_a black olimp_desc")
-        studentClasses = node.find("span", class_="classes_dop")
-        subjectTags = node.find_all("span", class_="subject_tag")
-        url = node.find("a", class_="none_a black").get_attribute_list("href")[0]
-        
-        olimpicData = {
-            "title": title.text if title is not None else "",
-            "raitng": raiting.text if raiting is not None else "",
-            "description": description.text if description is not None else "",
-            "studentClasses": studentClasses.text if studentClasses is not None else "",
-            "subjectTags": ", ".join(map(lambda x: x.text.strip(), subjectTags)) if subjectTags is not None else "",
-            "url": f"https://olimpiada.ru{url}" if url is not None else ""
-        }
+        response = []
 
-        return olimpicData
+        for node in nodeList:
+            title = node.find("span", class_="headline")
+            raiting = node.find("span", class_="pl_rating")
+            description = node.find("a", class_="none_a black olimp_desc")
+            studentClasses = node.find("span", class_="classes_dop")
+            subjectTags = node.find_all("span", class_="subject_tag")
+            url = node.find("a", class_="none_a black").get_attribute_list("href")[0]
+            
+            olimpicData = {
+                "Название олимпиады": title.text if title is not None else "",
+                "Рейтинг": raiting.text if raiting is not None else "",
+                "Описание": description.text if description is not None else "",
+                "Классы": studentClasses.text if studentClasses is not None else "",
+                "Предмет(-ы)": ", ".join(map(lambda x: x.text.strip(), subjectTags)) if subjectTags is not None else "",
+                "Ссылка на олимпиаду": f"https://olimpiada.ru{url}" if url is not None else ""
+            }
+
+            response.append(olimpicData)
+
+        return response
+    
+    def saveDataToExcel(self, fileName: str, sheetName: str, preparedData: list[dict]) -> bool:
+        """Saving prepared data to excel"""
+
+        wb = Workbook()
+        ws = wb.active
+
+        try:
+            ws.title = sheetName
+            isCreateTableColumnNames = False
+            # Заполнение таблицы данными
+            for rowIndex in range(len(preparedData)):
+                if not isCreateTableColumnNames:
+                        columnNames = list(preparedData[rowIndex].keys())
+                        ws.append(columnNames)
+
+                        # Стилизация заголовков таблицы
+                        for columnIndex in range(len(columnNames)):
+                            cell = ws.cell(row=1, column=columnIndex + 1)
+                            cell.fill = PatternFill("solid", fgColor="78c4ff")
+                            ws.column_dimensions[get_column_letter(columnIndex + 1)].width = 30
+
+                isCreateTableColumnNames = True
+                ws.append(list(preparedData[rowIndex].values()))
+
+
+            # Стилизация готовой таблицы (выравнивание текста, границы)
+            totalColumns = len(preparedData[0])
+            totalRows = len(preparedData) + 1
+            for rowIndex in range(totalRows):
+                for columnIndex in range(totalColumns):
+                    currentCell = ws.cell(row=rowIndex + 1,column=columnIndex + 1)
+                    currentCell.alignment = Alignment(horizontal="center", vertical="center", wrap_text=True)
+                    thins = Side(border_style="medium", color="080808")
+                    currentCell.border = Border(top=thins, bottom=thins, left=thins, right=thins)
+
+            wb.save(fileName)
+            return True
+        
+        except Exception as error:
+
+            print(error)
+            return False
+
 
 def main():
     url = "https://olimpiada.ru/activities"
@@ -52,10 +115,14 @@ def main():
         return 0
 
     nodeElementList = parser.getNodeElementList(markup)
+    preparedData = parser.parseDataFromNodeList(nodeList=nodeElementList)
 
-    for node in nodeElementList:
-        data = parser.parseDataFromNode(node)
-        print(data)
+    response = parser.saveDataToExcel("OlimpiadaParse.xlsx", "Список олимпиад", preparedData)
+
+    if response: 
+        print("[OK] Данные успешно получены и сохранены")
+    else:
+        print("[ERROR] Не удалось получить и сохранить данные")
 
 if __name__ == "__main__":
     main()
